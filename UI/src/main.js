ce3a5086e15c89cfcc52620ffed24ae677fc910d
EliranYaish <eliran1099@gmail.com>
2018-07-06 11:00:40 +0300

    add vue-router

--- UI/src/main.js
+++ UI/src/main.js
@@ -1,6 +1,11 @@
+//import the vue instance
 import Vue from 'vue'
-import App from './App.vue'
-
+//import the App component
+import App from './App'
+//import the vue router
+import VueRouter from 'vue-router'
+//tell vue to use the router
+Vue.use(VueRouter)
 new Vue({
   el: '#app',
   render: h => h(App)

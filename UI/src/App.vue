fdb965e3e381b42d1469f811e356a049a10941e4
EliranYaish <eliran1099@gmail.com>
2018-07-09 18:00:38 +0300

    delete UI project

--- UI/src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div id="app">
-
-    <router-link :to="{ name: 'UserList' }">Users List</router-link>
-    <router-link to="/newUser">New User</router-link>
-    <router-view></router-view>
-  </div>
-</template>
